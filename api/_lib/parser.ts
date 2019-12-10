f2784952e856eed183e3b05a1342832c34fba7b4
Prateek Surana <prateeksurana3255@gmail.com>
2019-12-10 14:43:27 +0530

    :recycle: Decode URI before parsing

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -4,7 +4,7 @@ import { ParsedRequest, Theme } from './types';
 
 export function parseRequest(req: IncomingMessage) {
     console.log('HTTP ' + req.url);
-    const { pathname = '/', query = {} } = parse(req.url || '', true);
+    const { pathname = '/', query = {} } = parse(req.url && decodeURIComponent(req.url) || '', true);
     const { fontSize, images, widths, heights, theme, md } = query;
 
     if (Array.isArray(fontSize)) {
